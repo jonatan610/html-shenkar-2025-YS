f11119d5f7b4eab5d093069c75e4cc3aa4db2f30
jonatan610 <yonatan610@gmail.com>
2025-07-29 23:03:22 +0300

    fix isuue courier list

--- TrackMe/server/seedCouriers.js
+++ TrackMe/server/seedCouriers.js
@@ -12,11 +12,19 @@ mongoose.connect(process.env.MONGO_URI)
     await Courier.deleteMany(); // Optional: clear existing couriers
 
     const couriers = [
-      { fullName: "BLD Courier 1", email: "courier1@trackme.com", password: "Pass123!" },
-      { fullName: "BLD Courier 2", email: "courier2@trackme.com", password: "Pass234!" },
-      { fullName: "BLD Courier 3", email: "courier3@trackme.com", password: "Pass345!" },
-      { fullName: "BLD Courier 4", email: "courier4@trackme.com", password: "Pass456!" },
-      { fullName: "BLD Courier 5", email: "courier5@trackme.com", password: "Pass567!" }
+   { fullName: "Courier 1",  email: "courier1@trackme.com",  password: "Pass123!" },
+{ fullName: "Courier 2",  email: "courier2@trackme.com",  password: "Pass234!" },
+{ fullName: "Courier 3",  email: "courier3@trackme.com",  password: "Pass345!" },
+{ fullName: "Courier 4",  email: "courier4@trackme.com",  password: "Pass456!" },
+{ fullName: "Courier 5",  email: "courier5@trackme.com",  password: "Pass567!" },
+{ fullName: "Courier 6",  email: "courier6@trackme.com",  password: "Pass678!" },
+{ fullName: "Courier 7",  email: "courier7@trackme.com",  password: "Pass789!" },
+{ fullName: "Courier 8",  email: "courier8@trackme.com",  password: "Pass890!" },
+{ fullName: "Courier 9",  email: "courier9@trackme.com",  password: "Pass901!" },
+{ fullName: "Courier 10", email: "courier10@trackme.com", password: "Pass012!" }
+
+   
+   
     ];
 
     // Hash passwords before inserting
